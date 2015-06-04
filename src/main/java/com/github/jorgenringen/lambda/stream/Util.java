38a8b4c3c268961c9c502e719509b439477bfb2c
Jorgen Ringen <jorgen.ringen@accenture.com>
2015-06-04 22:18:04 +0200

    Removed solution

--- src/main/java/com/github/jorgenringen/lambda/stream/Util.java
+++ src/main/java/com/github/jorgenringen/lambda/stream/Util.java
@@ -1,5 +1,7 @@
 package com.github.jorgenringen.lambda.stream;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.Comparator;
 import java.util.IntSummaryStatistics;
 import java.util.List;
@@ -9,83 +11,43 @@ import static java.util.stream.Collectors.*;
 
 public class Util {
 
-    public static List<String> mapToUppercase(List<String> strings) {
-        return strings.stream()
-                .map(String::toUpperCase)
-                .collect(toList());
-    }
+    public static List<String> mapToUppercase(List<String> input) {
+        throw new NotImplementedException();    }
 
-    public static List<String> removeElementsWithMoreThanFourCharacters(List<String> collection) {
-        return collection.stream()
-                .filter(s -> s.length() < 4)
-                .collect(toList());
-    }
+    public static List<String> removeElementsWithMoreThanFourCharacters(List<String> input) {
+        throw new NotImplementedException();    }
 
-    public static List<String> sortStrings(List<String> values) {
-        return values.stream()
-                .sorted(Comparator.naturalOrder())
-                .collect(toList());
-    }
+    public static List<String> sortStrings(List<String> input) {
+        throw new NotImplementedException();    }
 
-    public static List<Integer> sortIntegers(List<String> values) {
-        return values.stream()
-                .map(Integer::valueOf)
-                .sorted()
-                .collect(toList());
-    }
+    public static List<Integer> sortIntegers(List<String> input) {
+        throw new NotImplementedException();    }
 
-    public static List<Integer> sortIntegersDescending(List<String> values) {
-        return values.stream()
-                .map(Integer::valueOf)
-                .sorted(Comparator.<Integer>reverseOrder())
-                .collect(toList());
-    }
+    public static List<Integer> sortIntegersDescending(List<String> input) {
+        throw new NotImplementedException();    }
 
     public static Integer sum(List<Integer> numbers) {
-        return numbers.stream()
-                .mapToInt(Integer::intValue)
-                .sum();
+        throw new NotImplementedException();    }
+
+    public static List<String> flattenToSingleCollection(List<List<String>> input) {
+        throw new NotImplementedException();
     }
 
-    public static List<String> flattenToSingleCollection(List<List<String>> collection) {
-        return collection.stream()
-                .flatMap(List::stream)
-                .collect(toList());
-    }
+    public static String separateNamesByComma(List<Person> input) {
+        throw new NotImplementedException();    }
 
-    public static String separateNamesByComma(List<Person> collection) {
-        return collection.stream()
-                .map(Person::getName)
-                .collect(joining(", ", "Names: ", "."));
-    }
+    public static String findNameOfOldestPerson(List<Person> input) {
+        throw new NotImplementedException();    }
 
-    public static String findNameOfOldestPerson(List<Person> collection) {
-        return collection.stream()
-                .max(Comparator.comparingInt(Person::getAge))
-                .get()
-                .getName();
-    }
+    public static List<String> filterPeopleLessThan18YearsOld(List<Person> input) {
+        throw new NotImplementedException();    }
 
-    public static List<String> filterPeopleLessThan18YearsOld(List<Person> collection) {
-        return collection.stream()
-                .filter(p -> p.getAge() < 18)
-                .map(Person::getName)
-                .collect(toList());
-    }
+    public static IntSummaryStatistics getSummaryStatisticsForAge(List<Person> input) {
+        throw new NotImplementedException();    }
 
-    public static IntSummaryStatistics getSummaryStatisticsForAge(List<Person> collection) {
-        return collection.stream()
-                .mapToInt(Person::getAge)
-                .summaryStatistics();
-    }
+    public static Map<Boolean, List<Person>> partitionAdults(List<Person> input) {
+        throw new NotImplementedException();    }
 
-    public static Map<Boolean, List<Person>> partitionAdults(List<Person> collection) {
-        return collection.stream()
-                .collect(partitioningBy(p -> p.getAge() > 18));
-    }
-
-    public static Map<String, List<Person>> partitionByNationality(List<Person> collection) {
-        return collection.stream()
-                .collect(groupingBy(Person::getCountry));
-    }
+    public static Map<String, List<Person>> partitionByNationality(List<Person> input) {
+        throw new NotImplementedException();    }
 }
