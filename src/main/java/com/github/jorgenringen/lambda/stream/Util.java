d58547a4a094307c0ce138286866dfafb4294c76
Thiago Matos <thiago.matos@fpf.br>
2017-08-18 09:41:53 -0400

    removendo as respostas

--- src/main/java/com/github/jorgenringen/lambda/stream/Util.java
+++ src/main/java/com/github/jorgenringen/lambda/stream/Util.java
@@ -1,104 +1,63 @@
 package com.github.jorgenringen.lambda.stream;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
 
 public class Util {
 
     public static List<String> mapToUppercase(List<String> input) {
-        return input
-                .stream()
-                .map(String::toUpperCase)
-                .collect(Collectors.toList());
+        throw new NotImplementedException();
     }
 
     public static List<String> removeElementsWithMoreThanFourCharacters(List<String> input) {
-        return input
-                .stream()
-                .filter(str -> str.length() < 4)
-                .collect(Collectors.toList());
+        throw new NotImplementedException();
     }
 
     public static List<String> sortStrings(List<String> input) {
-        return input
-                .stream()
-                .sorted()
-                .collect(Collectors.toList());
+        throw new NotImplementedException();
     }
 
     public static List<Integer> sortIntegers(List<String> input) {
-        return input
-                .stream()
-                .map(Integer::parseInt)
-                .sorted()
-                .collect(Collectors.toList());
+        throw new NotImplementedException();
     }
 
     public static List<Integer> sortIntegersDescending(List<String> input) {
-        return input
-                .stream()
-                .map(Integer::parseInt)
-                .sorted(Comparator.reverseOrder())
-                .collect(Collectors.toList());
+        throw new NotImplementedException();
     }
 
     public static Integer sum(List<Integer> numbers) {
-        return numbers
-                .stream()
-                .reduce(0, (x, y) -> x + y);
+        throw new NotImplementedException();
     }
 
     public static List<String> flattenToSingleCollection(List<List<String>> input) {
-        return input
-                .stream()
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
+        throw new NotImplementedException();
     }
 
     public static String separateNamesByComma(List<Person> input) {
-        return input
-                .stream()
-                .map(Person::getName)
-                .collect(Collectors.joining(", ", "Names: ", "."));
+        throw new NotImplementedException();
     }
 
     public static String findNameOfOldestPerson(List<Person> input) {
-        return input
-                .stream()
-                .max(Comparator.comparing(Person::getAge))
-                .get()
-                .getName();
+        throw new NotImplementedException();
     }
 
     public static List<String> filterPeopleLessThan18YearsOld(List<Person> input) {
-        return input
-                .stream()
-                .filter(person -> person.getAge() < 18)
-                .map(Person::getName)
-                .collect(Collectors.toList());
+        throw new NotImplementedException();
     }
 
     public static IntSummaryStatistics getSummaryStatisticsForAge(List<Person> input) {
-        return input
-                .stream()
-                .mapToInt(Person::getAge)
-                .summaryStatistics();
+        throw new NotImplementedException();
     }
 
     public static Map<String, List<Person>> partitionByNationality(List<Person> input) {
-        return input
-                .stream()
-                .collect(
-                        Collectors.groupingBy(Person::getCountry)
-                );
+        throw new NotImplementedException();
     }
 
     public static Map<Boolean, List<Person>> partitionAdults(List<Person> input) {
-        return input
-                .stream()
-                .collect(
-                        Collectors.partitioningBy(person -> person.getAge() > 18)
-                );
+        throw new NotImplementedException();
     }
 
 }
